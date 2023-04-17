31dd8467216550e83f9e52d6980b09402c46fdac
Nhan Bach <>
2023-04-16 21:33:29 -0700

    add global field for xtheme

--- src/ui-components/createTheme.tsx
+++ src/ui-components/createTheme.tsx
@@ -12,23 +12,36 @@ declare module 'styled-components' {
 export const DEFAULT_THEME: DefaultTheme = {
     XComponent: {
         input: {
-            borderRadius: '0',
             caret: '_',
             space: '␣',
-            defaultWidth: '15em',
-            padding: '.7em .7em',
-            color: 'currentColor',
-            borderColor: 'currentColor',
-            caretColor: 'currentColor',
-        }
-
+        },
+        global: {
+            border: {
+                color: '#edf2f4',
+                radius: '0'
+            },
+            color: {
+                error: '#d90429',
+                warning: '#fb8500',
+                success: '#06d6a0',
+                primary: '#fca311',
+                secondary: '#0077b6'
+            },
+            text: '#edf2f4',
+            background: '#2b2d42',
+            padding: '.7em 1em'
+        },
+        __borderColor: (component) => DEFAULT_THEME.XComponent?.[component]?.borderColor??DEFAULT_THEME.XComponent?.global?.border?.color??'currentColor',
+        __borderRadius: (component) => DEFAULT_THEME.XComponent?.[component]?.borderRadius??DEFAULT_THEME.XComponent?.global?.border?.radius??'0',
+        __color: (component) => DEFAULT_THEME.XComponent?.[component]?.color??DEFAULT_THEME.XComponent?.global?.text??'currentColor',
+        __padding: (component) => DEFAULT_THEME.XComponent?.[component]?.padding??DEFAULT_THEME.XComponent?.global?.padding??'0'
     }
         
 }
 
 
 
-export function createTheme(theme: XTheme = {}) {
+export function createTheme(theme: Pick<XTheme, "button" | "input" | "global"> = {}) {
     const newTheme = _.merge(DEFAULT_THEME, {XComponent: theme})
     const ComponentThemeProvider = (props: React.PropsWithChildren<{}>) => {
         return <ThemeProvider theme={newTheme}>
@@ -40,5 +53,5 @@ export function createTheme(theme: XTheme = {}) {
 
 export function useXTheme() {
     const theme = useTheme()
-    return theme.XComponent
+    return theme ? theme.XComponent : DEFAULT_THEME.XComponent
 }
