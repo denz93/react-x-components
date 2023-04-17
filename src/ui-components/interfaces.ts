0bad0eba4dddfa4b94d587a6c986e9d47f639a49
Nhan Bach <>
2023-04-16 21:34:17 -0700

    add global field for xtheme

--- src/ui-components/interfaces.ts
+++ src/ui-components/interfaces.ts
@@ -9,11 +9,36 @@ export interface XTheme {
         borderColor?: string
         borderRadius?: string
     }
+    button?: {
+        color?: string 
+        borderColor?: string 
+        borderRadius?: string
+        padding?: string
+    }
     /**
      * "Global" styles will apply to all components
      */
     global? : {
-    }
+        color?: {
+            error?: string
+            success?: string
+            warning?: string
+            primary?: string
+            secondary?: string 
+        }
+        border?: {
+            radius?: string 
+            color?: string 
+        }
+        padding?: string
+        background?: string
+        text?: string
+    },
+
+    __borderColor: (component: 'input' | 'button') => string 
+    __borderRadius: (component: 'input' | 'button') => string 
+    __color: (component: 'input' | 'button') => string
+    __padding: (component: 'input' | 'button') => string
 }
 
 export interface IXBaseInputProps<T> {
